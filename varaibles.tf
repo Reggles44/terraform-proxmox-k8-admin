6d66a69322287436e97705c7c5dcb644c6adfb4e
Reggles <reginaldbeakes@gmail.com>
2025-01-08 14:04:28 -0500

    updates for terragrunt

--- varaibles.tf
+++ varaibles.tf
@@ -15,25 +15,11 @@ variable "proxmox_user" {
   type = string
 }
 
-variable "opnsense_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "opnsense_secret" {
-  type      = string
-  sensitive = true
-}
-
 variable "vmid" {
   type = number
 }
 
 variable "ip" {
-  type = string
-}
-
-variable "subnet_id" {
   type = string
 }
 
