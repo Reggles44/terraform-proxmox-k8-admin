d517d1eef1fb2bdf14824f437c42bb6914c8b792
Reggles <reginaldbeakes@gmail.com>
2025-01-09 21:52:20 -0500

    updates for terragrunt

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "proxmox_vm_qemu" "k8_admin" {
 
   os_type       = "cloud-init"
   cicustom      = "user=local:snippets/debian.yml"
-  ipconfig0     = "dhcp"
+  ipconfig0     = "ip=dhcp"
   agent_timeout = 300
 
   connection {
